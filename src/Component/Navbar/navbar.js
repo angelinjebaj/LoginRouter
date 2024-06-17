67224effaeaaaaff7df306e3cfdf741b9af4a030
angelinjebaj <angelinjebaj@gmail.com>
2024-06-17 07:23:00 +0530

    first commit

---
+++ src/Component/Navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+const Navbar = () => {
+  return (
+    <div className='nav flexRow fullW pdgLft'>
+      <img className='logoImg' src="/images/Screenshot_16-6-2024_19363_.jpeg" alt="My Image"/>
+      <h6>English</h6>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Component/Welcome/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  './welcome.css'
+const Welcome = () => {
+  return (
+    <div>
+      <h4>Welcome to inLynk !</h4>
+      <h2>Take your business to next <span className='levelText'> level</span></h2>
+      
+    </div>
+  )
+}
+
+export default Welcome
